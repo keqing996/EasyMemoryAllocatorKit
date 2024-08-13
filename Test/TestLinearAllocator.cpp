e7430642328d15b5029e8a5f53d3a6a051b39c19
keqing996 <epigrf@outlook.com>
2024-08-13 11:59:13 +0800

    Add doctest.

--- Test/TestLinearAllocator.cpp
+++ Test/TestLinearAllocator.cpp
@@ -1,11 +1,16 @@
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
 #include <iostream>
 #include "Allocator/LinearAllocator.h"
 
-int main()
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("TestExample")
 {
-    LinearAllocator allocator(128, 4);
-
-    void* pInt = allocator.Allocate(4);
-
-    std::cout << pInt << std::endl;
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(2) == 2);
+    CHECK(factorial(3) == 6);
+    CHECK(factorial(10) == 3628800);
 }
