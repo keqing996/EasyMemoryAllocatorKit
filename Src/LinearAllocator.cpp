e618083ddfa72172ac33631415eef5a1dcd4d425
keqing996 <epigrf@outlook.com>
2024-08-20 14:23:55 +0800

    Simplify linear allocator.

--- Src/LinearAllocator.cpp
+++ Src/LinearAllocator.cpp
@@ -3,44 +3,27 @@
 #include "Allocator/LinearAllocator.h"
 #include "Util.hpp"
 
-static constexpr size_t MIN_BLOCK_SIZE = 128;
-
-LinearAllocator::LinearAllocator(size_t minBlockSize, size_t defaultAlignment)
-    : _defaultAlignment(Util::UpAlignmentPowerOfTwo(defaultAlignment))
-    , _defaultBlockSize(minBlockSize < MIN_BLOCK_SIZE ? MIN_BLOCK_SIZE: minBlockSize)
-    , _pFirst(nullptr)
-    , _pTail(nullptr)
+LinearAllocator::LinearAllocator(size_t size)
+    : _pData(static_cast<uint8_t*>(::malloc(size)))
+    , _size(size)
 {
-    _defaultBlockSize = Util::UpAlignmentPowerOfTwo(_defaultBlockSize);
-    AddBlock(0);
 }
 
 LinearAllocator::~LinearAllocator()
 {
-    BlockHeader* pItr = _pFirst;
-    while (pItr != nullptr)
-    {
-        BlockHeader* pNeedToFree = pItr;
-        pItr = pItr->pNext;
-        ::free(pNeedToFree);
-    }
-}
-
-void* LinearAllocator::Allocate(size_t size)
-{
-    return Allocate(size, _defaultAlignment);
+    ::free(_pData);
 }
 
 void* LinearAllocator::Allocate(size_t size, size_t alignment)
 {
-    size_t alignedSize = Util::UpAlignment(size, alignment);
-    size_t available = _pTail->size - GetBlockUsedSize(_pTail);
+    size_t requiredSize = Util::UpAlignment(size, alignment);
+    size_t available = GetAvailableSpace();
 
-    if (available < alignedSize)
-        AddBlock(alignedSize);
+    if (available < requiredSize)
+       return nullptr;
 
-    void* result = _pTail->pCurrent;
-    _pTail->pCurrent = reinterpret_cast<void*>(alignedSize + reinterpret_cast<size_t>(_pTail->pCurrent));
+    void* result = _pCurrent;
+    _pCurrent += requiredSize;
     return result;
 }
 
@@ -49,84 +32,7 @@ void LinearAllocator::Deallocate(void* p)
     // do nothing
 }
 
-size_t LinearAllocator::GetCurrentAlignment() const
+size_t LinearAllocator::GetAvailableSpace() const
 {
-    return _defaultAlignment;
-}
-
-size_t LinearAllocator::GetDefaultBlockSize() const
-{
-    return _defaultBlockSize;
-}
-
-size_t LinearAllocator::GetCurrentBlockNum() const
-{
-    size_t result = 0;
-
-    BlockHeader* pItr = _pFirst;
-    while (pItr != nullptr)
-    {
-        result++;
-        pItr = pItr->pNext;
-    }
-
-    return result;
-}
-
-float LinearAllocator::CalculateOccupancyRate() const
-{
-    size_t total = 0;
-    size_t used = 0;
-    BlockHeader* pItr = _pFirst;
-    while (pItr != nullptr)
-    {
-        total += pItr->size;
-        used += GetBlockUsedSize(pItr);
-    }
-
-    return static_cast<float>(used) / total;
-}
-
-void LinearAllocator::AddBlock(size_t requiredSize)
-{
-    size_t blockContentSize = Util::UpAlignment(requiredSize > _defaultBlockSize
-        ? requiredSize : _defaultBlockSize, _defaultAlignment);
-
-    size_t totalSize = blockContentSize + Util::GetPaddedSize<BlockHeader>(_defaultAlignment);
-    
-    void* pMemory = ::malloc(totalSize);
-
-    BlockHeader* pBlock = static_cast<BlockHeader*>(pMemory);
-    pBlock->pCurrent = GetBlockStartPtr(pBlock);
-    pBlock->pNext = nullptr;
-    pBlock->size = blockContentSize;
-    
-    if (_pFirst == nullptr)
-    {
-        _pFirst = pBlock;
-        _pTail = pBlock;
-    }
-    else 
-    {
-        _pTail->pNext = pBlock;
-        _pTail = pBlock;
-    }
-}
-
- void* LinearAllocator::GetBlockStartPtr(const BlockHeader* pBlock) const
- {
-    size_t addrBlock = reinterpret_cast<size_t>(pBlock);
-    return reinterpret_cast<void*>(addrBlock + Util::GetPaddedSize<BlockHeader>(_defaultAlignment));
- }
-
-size_t LinearAllocator::GetBlockUsedSize(const BlockHeader* pBlock) const
-{
-    size_t addrBlock = reinterpret_cast<size_t>(pBlock);
-    size_t addrStart = addrBlock + Util::GetPaddedSize<BlockHeader>(_defaultAlignment);
-    return reinterpret_cast<size_t>(pBlock->pCurrent) - addrStart;
-}
-
-const LinearAllocator::BlockHeader* LinearAllocator::GetFirstBlockPtr() const
-{
-    return _pFirst;
+    return reinterpret_cast<size_t>(_pData) + _size - reinterpret_cast<size_t>(_pCurrent);
 }
